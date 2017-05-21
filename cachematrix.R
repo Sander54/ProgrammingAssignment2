74404e2e5521a89e2593802a90145f6832ad05f9
Sander54 <Cherli@zonnet.nl>
2017-05-21 16:43:05 +0200

    Transfer adjusted Script ChaceMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Return AND Cache a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
+# The first function below creates a list of functions for creating a Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invrs <- NULL
+  set <<- function(y) {
+    x <<- y
+    invrs <- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) invrs <<- inverse
+  getinverse <- function() invrs
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+# The following/second function creates the Inverse-Matrix, based on the "list/values" 
+# created in the first function (MakeCacheMatrix).
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  invrs <= x$getinverse()
+  if(!is.null(invrs)) {
+    message("getting cached data")
+    return(invrs)
+  }
+  mat <- x$get()
+  invrs <- solve(mat, ...)
+  x$setinverse(invrs)
+  invrs
 }
